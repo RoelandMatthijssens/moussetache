c982ba12a975f7793a87a334f11a8dc69ead58f8
Roeland Matthijssens <fulgens.ailurus@gmail.com>
2014-07-21 16:51:36 +0200

    restarting

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Moussetache::Application.config.secret_token = '94bbb496f51a80bb83511ee92a736dc538db6dcb22b7ff82fb4f32bb46a6a8e37097cb6a21683cf736d7a0cd798d655e68282dc1ccdd8867cca41704feb03c15'
--- vendor/extensions/dishes/spec/support/factories/refinery/dishes.rb
+++
@@ -1,7 +0,0 @@
-
-FactoryGirl.define do
-  factory :dish, :class => Refinery::Dishes::Dish do
-    sequence(:name) { |n| "refinery#{n}" }
-  end
-end
-
