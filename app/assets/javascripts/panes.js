6a1673aa1703d49aa8c805fc134d3063f71592bb
Roeland Matthijssens <fulgens.ailurus@gmail.com>
2014-08-30 17:12:02 +0200

    switched to static scaling for man layout

--- app/assets/javascripts/panes.js
+++ app/assets/javascripts/panes.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
+	resetPanelHeight();
+});
+
+function resetPanelHeight(){
 	var headerHeight = $(".header").height();
 	var bannerHeight = $(".top_banner").height();
 	var documentHeight = $(document).height();
-	var fullHeight = documentHeight - headerHeight+bannerHeight;
+	var fullHeight = documentHeight - (headerHeight+bannerHeight);
 	$(".panel").height(fullHeight);
+	console.log(headerHeight);
+	console.log(bannerHeight);
+	console.log(documentHeight);
 	console.log(fullHeight);
-});
+}
