9b2c8268116cac5714870dbcd99417a724f00acb
Roeland Matthijssens <fulgens.ailurus@gmail.com>
2014-08-31 23:22:39 +0200

    fixed panel height issue

--- app/assets/javascripts/panes.js
+++ app/assets/javascripts/panes.js
@@ -2,7 +2,7 @@ function resetPanelHeight(){
 	var headerHeight = $(".header").height();
 	var bannerHeight = $(".top_banner").height();
 	var documentHeight = $(document).height();
-	var fullHeight = documentHeight - (headerHeight+bannerHeight);
+	var fullHeight = documentHeight - (headerHeight+bannerHeight+22);
 	$(".panel").height(fullHeight);
 	console.log(headerHeight);
 	console.log(bannerHeight);
